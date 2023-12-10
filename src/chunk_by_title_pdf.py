2d8af9d62e6023954b4fa4f29f112d15f114824f
nanli <linanenv@gmail.com>
2023-12-10 15:14:12 +0000

    add vision

--- src/chunk_by_title_pdf.py
+++ src/chunk_by_title_pdf.py
@@ -1,20 +1,21 @@
-from dataclasses import dataclass
+import tempfile
 
 from unstructured.chunking.title import chunk_by_title
 from unstructured.cleaners.core import (
     clean,
-    clean_ordered_bullets,
     group_broken_paragraphs,
-    remove_punctuation,
 )
-from unstructured.documents.elements import ElementMetadata, Footer, Header, Text
+from unstructured.documents.elements import Footer, Header, Image, Text
 from unstructured.partition.auto import partition
+
+from tools.vision import vision_completion
 
 pdf_name = "raw/BYD_CSR_2022.pdf"
 
 elements = partition(
     filename=pdf_name,
-    pdf_extract_images=False,
+    pdf_extract_images=True,
+    pdf_image_output_dir_path=tempfile.gettempdir(),
     skip_infer_table_types=["jpg", "png", "xls", "xlsx"],
     strategy="hi_res",
 )
@@ -26,17 +27,18 @@ filtered_elements = [
 ]
 
 for element in filtered_elements:
-    if isinstance(element, Text):
+    if element.text != "":
         element.text = group_broken_paragraphs(element.text)
         element.text = clean(
             element.text,
-            bullets=True,
+            bullets=False,
             extra_whitespace=True,
-            dashes=True,
+            dashes=False,
             trailing_punctuation=False,
         )
-        element.text = clean_ordered_bullets(element.text)
-        # element.text = remove_punctuation(element.text)
+    elif isinstance(element, Image):
+        element.text = vision_completion(element.metadata.image_path)
+
 
 chunks = chunk_by_title(
     elements=filtered_elements,
---
+++ src/tools/vision.py
@@ -0,0 +1,40 @@
+import base64
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def vision_completion(image_path: str) -> str:
+    base64_image = encode_image(image_path)
+    response = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What’s in this image? Only return neat facts in English.",
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}",
+                        },
+                    },
+                ],
+            }
+        ],
+        max_tokens=300,
+    )
+    return response.choices[0].message.content
