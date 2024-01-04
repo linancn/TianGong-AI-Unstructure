f277e0d436802d64bfb775b7851b7fa7e18ffcaf
nanli <linanenv@gmail.com>
2024-01-04 13:39:29 +0800

    update

---
+++ src/tools/unlock.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+import pikepdf
+
+
+def unlock_file(file):
+    pdf = pikepdf.open(file, allow_overwriting_input=True)
+    pdf.save(file)
+
+
+def unlock_directory(folder="download"):
+    os.chdir(folder)
+    filelist = os.listdir()
+    for file in filelist:
+        if os.path.splitext(file)[1] == ".pdf":
+            unlock_file(file)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        unlock_directory()
+    else:
+        target = sys.argv[1]
+        if os.path.isdir(target):
+            unlock_directory(target)
+        else:
+            unlock_file(target)
+
+    print("done")
