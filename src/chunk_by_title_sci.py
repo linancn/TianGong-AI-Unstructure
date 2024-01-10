51f02d769b1c938635541046cf1c2a56c0d8d898
nanli <linanenv@gmail.com>
2024-01-11 00:53:48 +0800

    update

--- src/chunk_by_title_sci.py
+++ src/chunk_by_title_sci.py
@@ -1,5 +1,6 @@
 import concurrent.futures
 import os
+import time
 
 from tools.chunk_by_sci_pdf import sci_chunk
 
@@ -9,6 +10,11 @@ pdf_names = [os.path.join(directory, name) for name in os.listdir(directory)]
 
 # for pdf_name in pdf_names:
 #     sci_chunk(pdf_name)
+start_time = time.time()
 
 with concurrent.futures.ProcessPoolExecutor(max_workers=12) as executor:
     executor.map(sci_chunk, pdf_names)
+
+end_time = time.time()
+
+print(f"Execution time: {end_time - start_time} seconds")
