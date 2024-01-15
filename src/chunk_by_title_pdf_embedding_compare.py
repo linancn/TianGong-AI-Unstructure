d2d0892bffe5ace279a3bd5fe585166110bd6aee
nanli <linanenv@gmail.com>
2024-01-15 09:40:53 +0000

    update

--- src/chunk_by_title_pdf_embedding_compare.py
+++ src/chunk_by_title_pdf_embedding_compare.py
@@ -16,6 +16,6 @@ record_ids = set([record["id"] for record in records])
 
 download_files = set(os.path.splitext(file)[0] for file in os.listdir("pickle"))
 
-missing_in_download = record_ids- download_files
+missing_in_download = record_ids - download_files
 
 print(f"Missing in download: {len(missing_in_download)}")
