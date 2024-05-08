fdbe3565a6121915781ac732f16760a098e3c153
nanli <linanenv@gmail.com>
2024-05-08 23:18:36 +0800

    add reports

---
+++ src/reports/1_chunk_by_title.py
@@ -0,0 +1,84 @@
+import concurrent.futures
+import os
+import pickle
+
+from dotenv import load_dotenv
+from tools.unstructure_pdf import unstructure_pdf
+from xata.client import XataClient
+
+load_dotenv()
+
+xata = XataClient(
+    api_key=os.getenv("XATA_API_KEY"), db_url=os.getenv("XATA_DOCS_DB_URL")
+)
+
+table_name = "reports"
+columns = ["id", "language"]
+filter = {"$notExists": "embedding_time"}
+
+
+def fetch_all_records(xata, table_name, columns, filter, page_size=1000):
+    all_records = []
+    cursor = None
+    more = True
+
+    while more:
+        page = {"size": page_size}
+        if not cursor:
+            results = xata.data().query(
+                table_name,
+                {
+                    "page": page,
+                    "columns": columns,
+                    "filter": filter,
+                },
+            )
+        else:
+            page["after"] = cursor
+            results = xata.data().query(
+                table_name,
+                {
+                    "page": page,
+                    "columns": columns,
+                },
+            )
+
+        all_records.extend(results["records"])
+        cursor = results["meta"]["page"]["cursor"]
+        more = results["meta"]["page"]["more"]
+
+    return all_records
+
+
+records = fetch_all_records(xata, table_name, columns, filter)
+
+
+def process_pdf(record):
+    record_id = record["id"]
+    if record["language"] == "eng":
+        language = ["eng"]
+    else:
+        language = [record["language"], "eng"]
+
+    text_list = unstructure_pdf(
+        pdf_name="docs/reports/" + record_id + ".pdf", languages=language
+    )
+
+    with open("reports_pickle/" + record_id + ".pkl", "wb") as f:
+        pickle.dump(text_list, f)
+
+    text_str = "\n----------\n".join(text_list)
+
+    with open("reports_txt/" + record_id + ".txt", "w") as f:
+        f.write(text_str)
+
+
+# record = {"id": "rec_coh3s2o41648vkt35lsg", "language": "chi_sim"}
+
+# process_pdf(record)
+
+# for record in records:
+#     process_pdf(record)
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=16) as executor:
+    executor.map(process_pdf, records)
---
+++ src/reports/2_embedding_init.py
@@ -0,0 +1,237 @@
+import logging
+import os
+import pickle
+from datetime import UTC, datetime
+from io import StringIO
+
+import pandas as pd
+import tiktoken
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from openai import OpenAI
+from pinecone import Pinecone
+from tenacity import retry, stop_after_attempt, wait_fixed
+from xata import XataClient
+
+load_dotenv()
+
+logging.basicConfig(
+    filename="reports_embedding.log",
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+    filemode="w",
+    force=True,
+)
+
+client = OpenAI()
+
+xata_api_key = os.getenv("XATA_API_KEY")
+xata_db_url = os.getenv("XATA_DOCS_DB_URL")
+
+xata = XataClient(
+    api_key=xata_api_key,
+    db_url=xata_db_url,
+)
+
+pc = Pinecone(api_key=os.environ.get("PINECONE_SERVERLESS_API_KEY"))
+idx = pc.Index(os.environ.get("PINECONE_SERVERLESS_INDEX_NAME"))
+
+
+def fetch_all_records(xata, table_name, columns, filter, page_size=1000):
+    all_records = []
+    cursor = None
+    more = True
+
+    while more:
+        page = {"size": page_size}
+        if not cursor:
+            results = xata.data().query(
+                table_name,
+                {
+                    "page": page,
+                    "columns": columns,
+                    "filter": filter,
+                },
+            )
+        else:
+            page["after"] = cursor
+            results = xata.data().query(
+                table_name,
+                {
+                    "page": page,
+                    "columns": columns,
+                },
+            )
+
+        all_records.extend(results["records"])
+        cursor = results["meta"]["page"]["cursor"]
+        more = results["meta"]["page"]["more"]
+
+    return all_records
+
+
+def num_tokens_from_string(string: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding("cl100k_base")
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
+def fix_utf8(original_list):
+    cleaned_list = []
+    for original_str in original_list:
+        cleaned_str = original_str.replace("\ufffd", " ")
+        cleaned_list.append(cleaned_str)
+    return cleaned_list
+
+
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
+def get_embeddings(text_list, model="text-embedding-3-small"):
+    try:
+        text_list = [text.replace("\n\n", " ").replace("\n", " ") for text in text_list]
+        length = len(text_list)
+        results = []
+        # print(text_list[1128:1129])
+        # result = client.embeddings.create(input=text_list[1128:1129], model=model).data
+
+        for i in range(0, length, 1000):
+            result = client.embeddings.create(
+                input=text_list[i : i + 1000], model=model
+            ).data
+            results += result
+        return results
+
+    except Exception as e:
+        print(e)
+
+
+# [Embedding(embedding=[], index=0, object='embedding'),Embedding(embedding=[], index=0, object='embedding')]
+
+
+def load_pickle_list(file_path):
+    with open(file_path, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+
+def split_dataframe_table(html_table, chunk_size=8100):
+    dfs = pd.read_html(StringIO(html_table))
+    if not dfs:
+        return []
+
+    df = dfs[0]
+    tables = []
+    sub_df = pd.DataFrame()
+    token_count = 0
+
+    for _, row in df.iterrows():
+        row_html = row.to_frame().T.to_html(index=False, border=0, classes=None)
+        row_token_count = num_tokens_from_string(row_html)
+
+        if token_count + row_token_count > chunk_size and not sub_df.empty:
+            sub_html = sub_df.to_html(index=False, border=0, classes=None)
+            tables.append(sub_html)
+            sub_df = pd.DataFrame()
+            token_count = 0
+
+        sub_df = pd.concat([sub_df, row.to_frame().T])
+        token_count += row_token_count
+
+    if not sub_df.empty:
+        sub_html = sub_df.to_html(index=False, border=0, classes=None)
+        tables.append(sub_html)
+
+    return tables
+
+
+def merge_pickle_list(data):
+    temp = ""
+    result = []
+    for d in data:
+        if num_tokens_from_string(d) > 8100:
+            soup = BeautifulSoup(d, "html.parser")
+            tables = soup.find_all("table")
+            for table in tables:
+                table_content = str(table)
+                if num_tokens_from_string(table_content) < 8100:
+                    if table_content:  # 确保表格内容不为空
+                        result.append(table_content)
+                else:
+                    try:
+                        sub_tables = split_dataframe_table(table_content)
+                        for sub_table in sub_tables:
+                            if sub_table:
+                                soup = BeautifulSoup(sub_table, "html.parser")
+                                result.append(str(soup))
+                    except Exception as e:
+                        logging.error(e)
+        elif num_tokens_from_string(d) < 15:
+            temp += d + " "
+        else:
+            result.append(temp + d)
+            temp = ""
+    if temp:
+        result.append(temp)
+
+    return result
+
+
+@retry(wait=wait_fixed(3), stop=stop_after_attempt(10))
+def upsert_vectors(vectors):
+    try:
+        idx.upsert(
+            vectors=vectors, batch_size=200, namespace="report", show_progress=False
+        )
+    except Exception as e:
+        logging.error(e)
+
+
+table_name = "reports"
+columns = ["id"]
+filter = {"$all": [{"$notExists": "embedding_time"}]}
+
+all_records = fetch_all_records(xata, table_name, columns, filter)
+
+ids = [record["id"] for record in all_records]
+
+files = [id + ".pkl" for id in ids]
+
+dir = "reports_pickle"
+
+# aa = os.listdir(dir)
+
+for file in files:
+
+    try:
+        file_path = os.path.join(dir, file)
+        data = load_pickle_list(file_path)
+        data = merge_pickle_list(data)
+        data = fix_utf8(data)
+        embeddings = get_embeddings(data)
+
+        file_id = file.split(".")[0]
+
+        vectors = []
+        for index, e in enumerate(embeddings):
+            vectors.append(
+                {
+                    "id": file_id + "_" + str(index),
+                    "values": e.embedding,
+                    "metadata": {
+                        "text": data[index],
+                        "rec_id": file_id,
+                    },
+                }
+            )
+
+        upsert_vectors(vectors)
+
+        embedded = xata.records().update(
+            "reports", file_id, {"embedding_time": datetime.now(UTC).isoformat()}
+        )
+
+        logging.info(f"{file_id} embedding finished")
+
+    except Exception as e:
+        logging.error(e)
+        continue
